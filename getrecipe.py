2bd127508d1b61391b94e9545b5dbbf24d613fff
Louielo <56979318+Louielo@users.noreply.github.com>
2020-11-23 19:06:44 +0100

    Update getrecipe.py

--- getrecipe.py
+++ getrecipe.py
@@ -1,13 +1,11 @@
 
 from requests import get	# get pour émettre une requete HTTP
 from bs4 import BeautifulSoup	# il faut l'installer avant : pip install beautifulsoup4
-from random import randrange	# pour recette aléatoire
-
 
 tablink = []
 tabtext = []
 
-url = "https://www.marmiton.org/recettes/recherche.aspx?aqt="
+url = "https://www.marmiton.org/"
 response = get(url)
 
 if response.status_code == 200 : # on vérifie que la requete ai bien abouti
@@ -22,27 +20,23 @@ if response.status_code == 200 : # on vérifie que la requete ai bien abouti
 # 504 : le serveur n'a pas répondu.
 	soup = BeautifulSoup(source, "lxml") 	#précise qu'on veut le format lxml
 
-for link in soup.find_all("a", {"class": "recipe-card-link"}):
-	tablink.append(link.get('href'))
-else :
-	source = None
+	for link in soup.find_all("a", {"class": "header-main-menu__item"}):
+		tablink.append(link.get('href'))
 
-alea = randrange(15)
+	link = tablink[4]
 
-print(tablink[alea])	# on récupère un lien de recette aléatoire
+	url = "http://marmiton.org"+link # faudra trouver un autre moyen de récupérer le début
+	response = get(url)
 
-# print(source)
-
-url = "http://marmiton.org"+tablink[alea] # faudra trouver un autre moyen de récupérer le début
-response = get(url)
-
-if response.status_code == 200 : # on vérifie que la requete ai bien abouti
 	source = response.text
 	soup = BeautifulSoup(source, "lxml")
-	# print(soup)
-else :
-	source = None
 
+else :
+	print("Impossible de récupérer une recette sur le site de "+url)
+
+for title in soup.find_all("h1", {"class": "main-title"}):
+	title = title.get_text()
+	print("\n"+title+"\n")
 for text in soup.find_all("li", {"class": "recipe-preparation__list__item"}):
 	tabtext.append(text.get_text())
 	print(text.get_text())
