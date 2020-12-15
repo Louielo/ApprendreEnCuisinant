9c158e9dc57bf943628bbf6d640dcfa2a8e51bd7
Louielo <56979318+Louielo@users.noreply.github.com>
2020-12-15 22:04:10 +0100

    Add files via upload

--- record.py
+++ record.py
@@ -1,18 +1,28 @@
 
 import sounddevice as sd
 from scipy.io.wavfile import write
+from playsound import playsound 	# pour linux
 import os
 
-fs = 44100  # fréquence
-seconds = 5  # Durée de l'enregistrement
+fs = 22000  # fréquence par défaut qui ne prend pas trop de place sur le disque
+duree = 3  # Durée de l'enregistrement
 
-myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-print("Prononcez le mot..")
-sd.wait()  # on attend que l'enregistrement se termine
-print("Mot enregistré")
-write('record.wav', fs, myrecording)  # enregistre en wav
-os.startfile("record.wav")
+while True :	# tant que l'utilisateur n'est pas content de sa prononciation il peut se réenregistrer
+	record = sd.rec(int(duree * fs), samplerate=fs, channels=2) # commande pour enregistrer avec sounddevice
+
+	print("prononcez le mot ICILEMOT ...")
+	sd.wait()  # on attend que l'enregistrement se termine
+	write('record.wav', fs, record)  # enregistre en wav
+
+	# Sur windows : os.startfile("record.wav") # pour faire écouter à l'utilisateur ce qu'il a prononcé
+	# Sur linux : on utilise la bibliothèque playsound
+	playsound('record.wav')
+	avis = input("Voulez vous prononcer le mot encore une fois ? y/n ")
+	if avis == "n" :
+		break
+
 
 #commandes pour installer les bibliothèques utilisées
 #pip install sounddevice
 #pip install scipy
+#pip install playsound # pour linux
